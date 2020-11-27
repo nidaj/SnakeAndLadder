7341e88707235f5dff35f0c55c20a8aa5031f52e
nida.j <nida.jawre@gmail.com>
2020-11-27 14:25:25 +0530

    Player Intialised to 0 postion

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class SnakeAndLadder {
+  	final int ENDPOS=100;
+	final int STARTPOS=0;
+	int userPos;
+	int diceRoll;
+  	public SnakeAndLadder(){
+	    	userPos = STARTPOS;
+	}
+	public static void main(String[] args) {
+		SnakeAndLadder user = new SnakeAndLadder();
+	}
+}
